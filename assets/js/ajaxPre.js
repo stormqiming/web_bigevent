b3e8880d91d5791fc6ff2c3faf8dcedc255a7343
itheima <itheima@itcast.cn>
2022-02-11 23:25:27 +0800

    完成了登录和注册功能的开发

--- assets/js/ajaxPre.js
+++ assets/js/ajaxPre.js
@@ -1,16 +1,7 @@
-// 每次发起$.ajax或$.post或$.get请求之前，都会先调用$.ajaxPrefilter
-// $.ajaxPrefilter的options 参数时 $.ajax或$.post或$.get的对象
+//注意：每次调用$.get()或$.post或$.ajax()的时候，会先调用ajaxPrefilter这个函数，在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function(options) {
-    if (options.url.startsWith('/my')) {
-        options.headers = { Authorization: localStorage.getItem('token') || '' }
-
-        options.complete = function(res) {
-            if (res.responseJSON.status === 1 && res.responseJSON.msg === '身份认证失败') {
-                localStorage.removeItem('token')
-                location.href = '/login.html'
-            }
-        }
-    }
-
-    options.url = 'http://127.0.0.1:3007' + options.url
-})
+    console.log(options.url);
+    //在发起真正的Ajax请求之前，统一拼接请求的根路径
+    // options.url = 'http://www.liulongbin.top:3007' + options.url;
+    // console.log(options.url);
+});
--- assets/js/login.js
+++ assets/js/login.js
@@ -14,6 +14,7 @@ $(function() {
 
     //从layui中 获取form对象
     var form = layui.form;
+    var layer = layui.layer;
     //通过form.verify()函数自定义校验规则
     form.verify({
         //自定义了一个叫做pwd的校验规则 \S(非空格)
@@ -37,11 +38,37 @@ $(function() {
         //1.阻止默认的提交行为
         e.preventDefault();
         //2.发起Ajax的POST请求
-        $.post('http://www.liulongbin.top:3007/api/reguser', { username: $('#form_reg [name=username]').val(), password: $('#form_reg [name=password]').val() }, function(res) {
+        var data = { username: $('#form_reg [name=username]').val(), password: $('#form_reg [name=password]').val() };
+        $.post('http://www.liulongbin.top:3007/api/reguser', data, function(res) {
             if (res.status !== 0) {
-                return console.log(res.message);
+                return layer.msg(res.message);
             }
-            console.log('注册成功');
+            layer.msg('注册成功');
+        });
+
+        //模拟人的点击行为
+        $('#link_login').click();
+    });
+
+    //监听登录表单的提交事件
+    $('#form_login').submit(function(e) {
+        //阻止默认提交行为
+        e.preventDefault();
+        $.ajax({
+            url: 'http://www.liulongbin.top:3007/api/login',
+            method: 'POST',
+            //快速获取表单中的数据
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败');
+                }
+                layer.msg('登录成功');
+                //将登录成功得到的token字符串，保存到localStorage中
+                localStorage.setItem('token', res.token);
+                //跳转到后台主页
+                location.href = '/index.html';
+            }
         });
     });
 });
