f723db85a8364ffe123290f7fa7a96cb0c31ff63
itheima <itheima@itcast.cn>
2022-02-13 17:32:38 +0800

    完成了主页的开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -67,7 +67,7 @@ $(function() {
                 //将登录成功得到的token字符串，保存到localStorage中
                 localStorage.setItem('token', res.token);
                 //跳转到后台主页
-                location.href = '/index.html';
+                location.href = 'index.html';
             }
         });
     });
